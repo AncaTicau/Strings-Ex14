43a130e809e7065ddd5187e2eba6219537d3c352
MariusA <anca.ticau@gmail.com>
2019-06-07 21:40:00 +0300

    Exercise 14

---
+++ Strings-Ex14/Strings-Ex14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Strings_Ex14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "Toamna,iarna,primavara,vara";
+            Console.WriteLine(GetLongestWordLength(givenString));
+        }
+        //Write a method that takes a list of words and returns the length of the longest one.
+
+        public static int GetLongestWordLength(string input)
+        {
+            string[] words = input.Split(',');
+
+            string longestWord = words.OrderByDescending(x => x.Length).First();
+
+            return longestWord.Length;
+        }
+    }
+}
